f04ce356baa7f7546ddf01d0ac2a410e35015314
James Garcia <conectis360@gmail.com>
2025-05-08 23:39:55 -0300

    feat: calling user menu on header bar

--- src/components/header/ChatHeaderBar.tsx
+++ src/components/header/ChatHeaderBar.tsx
@@ -1,9 +1,8 @@
-import React, { useCallback } from "react";
-import { AppBar, Toolbar, Button, Box } from "@mui/material"; // Importe Box
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import ModelSelector from "./ModelSelector";
-import UserInfo from "./UserInfo";
-import { Link } from "react-router-dom";
+import UserMenu from "../userDarshboard/UserMenu"; // Importe o UserMenu
 
 const Spacer = styled("div")(({ theme }) => ({
   flexGrow: 1,
@@ -12,32 +11,29 @@ const Spacer = styled("div")(({ theme }) => ({
 interface ChatHeaderBarProps {
   onModelChange: (modelInfo: { modelName: string; newChat: boolean }) => void;
   userName: string | null;
+  userEmail: string | null;
   userAvatarUrl: string | null | undefined;
+  onLogOff: () => void; // Adicione a prop para o log off
 }
 
 const ChatHeaderBar: React.FC<ChatHeaderBarProps> = ({
   onModelChange,
   userName,
+  userEmail,
   userAvatarUrl,
+  onLogOff,
 }) => {
   return (
     <AppBar position="static" color="default" elevation={1}>
       <Toolbar>
         <ModelSelector onModelChange={onModelChange} />
         <Spacer />
-        <Box
-          component={Link}
-          to="/user-dashboard"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            textDecoration: "none",
-            color: "inherit",
-            "&:hover": { opacity: 0.8 },
-          }}
-        >
-          <UserInfo userName={userName} userAvatarUrl={userAvatarUrl} />
-        </Box>
+        <UserMenu
+          userName={userName}
+          userEmail={userEmail}
+          userAvatarUrl={userAvatarUrl}
+          onLogOff={onLogOff}
+        />
       </Toolbar>
     </AppBar>
   );
